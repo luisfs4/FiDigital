e1102118e3ebaa367bf8606a680071e031b40ae0
Luis Sanchez <luissanchezb35@gmail.com>
2023-04-03 13:57:01 -0600

    Cambiar logo default de usuario notificacion

--- app/Views/panel/base/menu.php
+++ app/Views/panel/base/menu.php
@@ -116,7 +116,7 @@
 									<a class="dropdown-item border-radius-md" href="javascript:;">
 										<div class="d-flex py-1">
 											<div class="my-auto">
-												<img src="<?= base_url('public/imagenes/mejora_regulatoria.png'); ?>" class="avatar avatar-sm  me-3'); ?>">
+												<img src="<?= base_url('public/imagenes/logo.png'); ?>" class="avatar avatar-sm  me-3'); ?>">
 											</div>
 											<div class="d-flex flex-column justify-content-center">
 												<h6 class="text-sm font-weight-normal mb-1">
